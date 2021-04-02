6584c532087e3ae4c84436232097ea126023eb49
Peter Chapman <peter@conglomo.co.nz>
2021-04-02 17:08:52 +1300

    Added support for local storage of the rendering model

--- GoToBible.Web/Client/Program.cs
+++ GoToBible.Web/Client/Program.cs
@@ -9,6 +9,7 @@ namespace GoToBible.Web.Client
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Blazored.LocalStorage;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -29,6 +30,7 @@ namespace GoToBible.Web.Client
             WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
