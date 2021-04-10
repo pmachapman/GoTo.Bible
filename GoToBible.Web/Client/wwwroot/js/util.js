6e1bfe7859a7f7d5df8c45d84d7f26de8531585b
Peter Chapman <peter@conglomo.co.nz>
2021-04-10 15:03:00 +1200

    The nav menu now collapses when you press go, next, and previous.

--- GoToBible.Web/Client/wwwroot/js/util.js
+++ GoToBible.Web/Client/wwwroot/js/util.js
@@ -1,6 +1,9 @@
 ﻿window.changeUrl = function (url) {
     history.pushState(null, '', url);
 }
+window.closeMenu = function (id) {
+    $('#' + id).collapse('hide');
+}
 window.showDialog = function (id) {
     $('#' + id).modal();
 }
