f625af50e0dca83ebc89fac61e44ac29058b7070
Peter Chapman <peter@conglomo.co.nz>
2022-09-14 16:08:44 +1200

    Fixed code formatting

--- GoToBible.Model/InterlinearMode.cs
+++ GoToBible.Model/InterlinearMode.cs
@@ -4,34 +4,33 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace GoToBible.Model
+namespace GoToBible.Model;
+
+using System;
+
+/// <summary>
+/// The interlinear mode.
+/// </summary>
+[Flags]
+public enum InterlinearMode
 {
-    using System;
+    /// <summary>
+    /// No special interlinear settings.
+    /// </summary>
+    None = 0,
 
     /// <summary>
-    /// The interlinear mode.
+    /// The interlinear ignores case.
     /// </summary>
-    [Flags]
-    public enum InterlinearMode
-    {
-        /// <summary>
-        /// No special interlinear settings.
-        /// </summary>
-        None = 0,
+    IgnoresCase = 1,
 
-        /// <summary>
-        /// The interlinear ignores case.
-        /// </summary>
-        IgnoresCase = 1,
+    /// <summary>
+    /// The interlinear ignores diacritics.
+    /// </summary>
+    IgnoresDiacritics = 2,
 
-        /// <summary>
-        /// The interlinear ignores diacritics.
-        /// </summary>
-        IgnoresDiacritics = 2,
-
-        /// <summary>
-        /// The interlinear ignores punctuation.
-        /// </summary>
-        IgnoresPunctuation = 4,
-    }
+    /// <summary>
+    /// The interlinear ignores punctuation.
+    /// </summary>
+    IgnoresPunctuation = 4,
 }
