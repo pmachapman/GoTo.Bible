c26ffe4a77b40ab45f66b3f5515492e7656cfce6
Peter Chapman <peter@conglomo.co.nz>
2026-06-17 07:30:53 +1200

    Normalize translation languages

--- GoToBible.Providers/ExtensionMethods.cs
+++ GoToBible.Providers/ExtensionMethods.cs
@@ -31,8 +31,12 @@ public static partial class ExtensionMethods
         language switch
         {
             "Arabic, Standard" => "Arabic",
+            "English: USA" => "English",
             "German, Standard" => "German",
             "Greek, Ancient" => "Greek",
+            "Greek, Koine" => "Greek",
+            "Hebrew, Ancient" => "Hebrew",
+            "Hebrew, Modern" => "Hebrew",
             _ => language,
         };
 
