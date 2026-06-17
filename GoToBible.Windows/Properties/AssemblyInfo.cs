13084c6dd166c7cdd651462952c7fe196f2ef12e
Peter Chapman <peter@conglomo.co.nz>
2026-06-17 16:49:42 +1200

    Update copyright message

--- GoToBible.Windows/Properties/AssemblyInfo.cs
+++ GoToBible.Windows/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="Conglomo">
-// Copyright 2020-2025 Conglomo Limited. Please see LICENSE for license details.
+// Copyright 2020-2026 Conglomo Limited. Please see LICENSE for license details.
 // </copyright>
 // -----------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Conglomo")]
 [assembly: AssemblyProduct("GoTo.Bible")]
-[assembly: AssemblyCopyright("Copyright 2020-2025 Conglomo Limited. Please see LICENSE for license details.")]
+[assembly: AssemblyCopyright("Copyright 2020-2026 Conglomo Limited. Please see LICENSE for license details.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
